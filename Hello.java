f92efb80928958d523d75ed9dca12dbd0f8bbba0
Vaishali-Tapaswi <52520564+Vaishali-Tapaswi@users.noreply.github.com>
2023-10-17 16:22:12 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld at 4.11");
+		System.out.println("HelloWorld at 4.22");
 	}
 
 }
